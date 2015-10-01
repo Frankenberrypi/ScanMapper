cebd17037bb22327bb63d093799c121556cf4540
Jeffrey Beckman <JeffreyJBeckman@gmail.com>
2015-10-01 06:49:41 -0700

    Switch to using continuous color mapping.

--- gpxWriter.py
+++ gpxWriter.py
@@ -1,31 +1,50 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Import data from file to numpy array, delete header.
 # Data is of the form lad,lon,signal strength
 scanData = np.genfromtxt('MapPoints.csv', delimiter=',')
+# Delete the header row
 scanData = np.delete(scanData,0,0)
 
-def colorBar(signalStrength):
-    if 200 < signalStrength <= 220:
-        return "FF0000"
-    elif 180 < signalStrength <= 200:
-        return "FF8000"
-    elif 160 < signalStrength <= 180:
-        return "FFFF00"
-    elif 140 < signalStrength <= 160:
-        return "00FF00"
-    elif 120 < signalStrength <= 140:
-        return "00FFFF"
-    elif 100 <= signalStrength <= 120:
-        return "0000FF"
-    else:
-        return False
+# Load the color map
+cMap = plt.get_cmap("jet")
 
+# This function takes an array of RGB color and returns a string of hex color
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
+# End of function -------------------------------------
+
+# This function takes a normalized signal strength and returns a string of 
+# hex color, using the function above. 
+def colorBar(signalStrength):
+    red = np.round(cMap(signalStrength)[0]*255,decimals=0)
+    green = np.round(cMap(signalStrength)[1]*255,decimals=0)
+    blue = np.round(cMap(signalStrength)[2]*255,decimals=0)
+    rgb = (red,green,blue)
+
+    return rgb_to_hex(rgb)
+# End of function -------------------------------------    
+    
+
+#Find the bounds for normalization
+latMax, lonMax, sigMax = scanData.max(axis=0)
+latMin, lonMin, sigMin = scanData.min(axis=0)
+
+#Do the normalization math, replacing the right column in the array
+for i in range(0,len(scanData)):
+    scanData[i,2] = (scanData[i,2] - sigMin) / (sigMax - sigMin)
+
+# Open the gpx file for writing
 outFile = "Scan 121.0-122.0MHz.gpx"
 fOut = open(outFile,"w")
-fOut.write('<?xml version="1.0" encoding="UTF-8"?><gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="CALTOPO">\n')
 
+# Write the first line of the gpx
+fOut.write('<?xml version="1.0" encoding="UTF-8"?><gpx xmlns="'\
+'http://www.topografix.com/GPX/1/1" version="1.1" creator="CALTOPO">\n')
+
+# Write the waypoints to the gpx file
 for i in range(0,len(scanData)):
     fOut.write('  <wpt lat="')
     fOut.write(np.array_str(scanData[i,0]))
@@ -41,6 +60,7 @@ for i in range(0,len(scanData)):
     fOut.write(']]></desc>\n')
     fOut.write('  </wpt>\n')
     
+# Close the gpx file, both with a tag, then close the file. 
 fOut.write('</gpx>\n')
 fOut.close()
 
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
 
+# Run the file reader, this generates a .csv file
 os.system("rfsReader.py")
+
+# Run the gpx writer, this generates a .gpx file from the .csv above
 os.system("gpxWriter.py")
 
-
+# Delete the .csv file to keep things tidy
 os.remove("MapPoints.csv")
